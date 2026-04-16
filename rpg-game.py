58e979b0b31864007dbc53ccc69d3428a56b9668
henrik8772 <henrik.johansson@elev.ga.ntig.se>
2026-04-16 09:49:10 +0200

    making buttons and changing colors

--- rpg-game.py
+++ rpg-game.py
@@ -17,21 +17,31 @@ root.geometry("800x600")
 
 custom_font = tkFont.Font(family="Arial", size=20)
 
+# Configs
+
+root.config(bg="black")
+
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=1)
+root.columnconfigure(2, weight=1)
+
 # The main loop
 
-main_menu_label = Label(root, text="RPG-WORLD", font=custom_font)
-main_menu_label.pack(pady=10)
+
+main_menu_label = Label(root, text="RPG-WORLD",
+                        font=custom_font, bg="black", fg="white")
+main_menu_label.grid(row=0, column=1, pady=25)
 
 start_game_button = Button(
-    root, text="START A NEW JOURNEY", width=25, font=custom_font, command=None)
-start_game_button.pack(pady=20)
+    root, text="START A NEW JOURNEY", width=25, font=custom_font, bg="black", activebackground="black", activeforeground="white", fg="white", command=None)
+start_game_button.grid(row=1, column=1, pady=20)
 
 credit_menu_button = Button(
-    root, text="CREDITS", width=25, font=custom_font, command=None)
-credit_menu_button.pack(pady=15)
+    root, text="CREDITS", width=25, font=custom_font, bg="black", activebackground="black", activeforeground="white", fg="white", command=None)
+credit_menu_button.grid(row=2, column=1, pady=15)
 
 exit_menu_button = Button(root, text="EXIT GAME",
-                          width=25, font=custom_font, command=None)
-exit_menu_button.pack(pady=15)
+                          width=25, font=custom_font, bg="black", activebackground="black", activeforeground="white", fg="white", command=None)
+exit_menu_button.grid(row=3, column=1, pady=15)
 
 root.mainloop()
