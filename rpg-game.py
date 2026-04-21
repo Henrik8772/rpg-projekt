a6891b6b86dd40e86ae0dabac74cd3550da781bd
henrik8772 <henrik.johansson@elev.ga.ntig.se>
2026-04-21 12:43:44 +0200

    trying to fix explore

--- rpg-game.py
+++ rpg-game.py
@@ -13,17 +13,65 @@ gold = 0
 potions = 0
 armor = 8
 mana = 50
+crit_rate = 20
+
+# Shop items
+
+potion = [
+    {
+        "name": "Healing Potion",
+        "heal": 20,
+        "gold": 3
+    },
+
+    {
+        "name": "Mana Potion",
+        "mana_regen": (5, 8),
+        "length": (2, 4),
+        "gold": 4
+    },
+
+    {
+        "name": "Strength Potion",
+        "damage": (3, 6),
+        "length": (1, 3),
+        "gold": 7
+    }
+]
+
+weapons = [
+    {
+        "name": "Battle Axe",
+        "damage": 20,
+        "gold": 345
+    },
+
+    {
+        "name": "A Damed Wish",
+        "damage": 670,
+        "gold": 9850,
+        # "ability": "Final Wish: The player sacrifices 99 percent of remaining Hp to deliver a final wish apon the world instantly killing all enemys near you, This ability wont be able to be used after it has been unleashed once already, instead you permanently gain Intertwined Wishes passive on your class."
+    }
+]
+# might add this or not using it as a reminder ik it wont work like this
+passives = [
+    {
+        "name": "Intertwined Wishes",
+        "gain": "All your attacks deal holy dmg and has a 10 percent chance to gain you 1 wish stack, at 10 wish stacks you instantly kill the next enemy and heal for the remaing hp of that said enemy."
+    }
+]
 
 
 # Tkinter
 
 root = Tk()
 root.title("RPG-WORLD")
-root.geometry("800x600")
+root.geometry("1000x800")
 
 # Custom fonts and text size
 
 custom_font = tkFont.Font(family="Arial", size=20)
+font_explore = tkFont.Font(family="Arial", size=15)
 
 # Configs
 
@@ -67,6 +115,20 @@ def return_menu():
 def explore():
     clear_screen()
 
+    label = Label(
+        root, text="WHICH PLACE SHALL WE ADVENTURE THROUGH?")
+    label.grid(row=0, column=1, pady=(75, 25))
+    button = Button(root, text="FOREST", command=forest)
+    button.grid(row=2, column=0, pady=(40, 15), padx=10)
+    button = Button(root, text="CAVERN", command=forest)
+    button.grid(row=2, column=1, pady=(40, 15), padx=10)
+    button = Button(root, text="MOUNTAINS", command=forest)
+    button.grid(row=2, column=2, pady=(40, 15), padx=10)
+
+
+def forest():
+    clear_screen()
+
 
 def shop():
     clear_screen()
@@ -87,8 +149,7 @@ def menu_stats():
     display_stats.insert(1, f"HEALTH: {player_hp}")
     display_stats.insert(2, f"ARMOR: {armor}")
     display_stats.insert(3, f"DAMAGE: {player_attack}")
-    display_stats.insert(4, "CRIT DAMAGE: ")
-    display_stats.insert(5, "CRIT RATE: ")
+    display_stats.insert(4, f"CRIT RATE: {crit_rate}%")
 
     display_stats.grid(row=1, column=1, pady=(40, 15))
 
